efd5e15b250e280511cde596294c54cdad881f2c
rddravic <rddravic@gmail.com>
2022-06-22 23:37:58 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+  x = '';
+
+  constructor() {}
+
+  ngOnInit() {
+    this.getLocation();
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(this.showPosition);
+    } else {
+      this.x = 'Geolocation is not supported by this browser.';
+    }
+  }
+
+  showPosition(position) {
+    this.x =
+      'Latitude: ' +
+      position.coords.latitude +
+      '<br>Longitude: ' +
+      position.coords.longitude;
+  }
 }
